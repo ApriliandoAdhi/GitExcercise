edceb56818e7251d7f2bf8edf095476896dee545
ApriliandoAdhi <129729445+ApriliandoAdhi@users.noreply.github.com>
2023-05-26 15:41:12 +0700

    Initial Commit

---
+++ GitExcercise/GitExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitExcerciseApp.swift
+//  GitExcercise
+//
+//  Created by Apriliando Adhi Rinaldy on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GitExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
